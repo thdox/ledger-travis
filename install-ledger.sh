04997cf5095ee5b2cd37614ecbc74c09cfc877af
thdox <thdox@free.fr>
2016-10-11 11:42:59 +0200

    Add ledger install on osx

--- install-ledger.sh
+++ install-ledger.sh
@@ -1,19 +1,28 @@
-sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
-sudo apt-get update -qq
-sudo apt-get install -qq gcc-4.8 g++-4.8
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 90
-sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 90
+#!/usr/bin/env bash
 
-sudo add-apt-repository ppa:boost-latest/ppa --yes
-sudo apt-get update -qq
-sudo apt-get install -qq libboost1.55-dev libboost-date-time1.55-dev libboost-filesystem1.55-dev libboost-iostreams1.55-dev libboost-regex1.55-dev libboost-test1.55-dev
+set -eu -o pipefail
 
-sudo apt-get install -qq libgmp-dev libmpfr-dev libedit-dev
+if [ "${TRAVIS_OS_NAME}" = "linux" ]; then
+    sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+    sudo apt-get update -qq
+    sudo apt-get install -qq gcc-4.8 g++-4.8
+    sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 90
+    sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 90
 
-curl -sL https://github.com/ledger/ledger/archive/master.tar.gz | tar xz
-cd ledger-master
+    sudo add-apt-repository ppa:boost-latest/ppa --yes
+    sudo apt-get update -qq
+    sudo apt-get install -qq libboost1.55-dev libboost-date-time1.55-dev libboost-filesystem1.55-dev libboost-iostreams1.55-dev libboost-regex1.55-dev libboost-test1.55-dev
 
-cmake .
-make -j2
+    sudo apt-get install -qq libgmp-dev libmpfr-dev libedit-dev
 
+    curl -sL https://github.com/ledger/ledger/archive/master.tar.gz | tar xz
+    cd ledger-master
 
+    cmake .
+    make -j2
+fi
+
+if [ "${TRAVIS_OS_NAME}" = "osx" ]; then
+    brew update
+    brew install ledger
+fi
